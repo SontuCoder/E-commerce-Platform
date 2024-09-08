3a9bf02c5bafd9a5162a304dd75e1a6831dd4491
SontuCoder <subhadipmaity791@gmail.com>
2024-09-09 01:01:31 +0530

    add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar.jsx'
+import { BrowserRouter,Routes,Route} from 'react-router-dom';
+import Shop from "./Pages/Shop.jsx";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import "./Navbar.css"
-const Navbar = () => {
+import React from 'react';
+import "./Navbar.css";
+import logo from "../Assest/icon/2-removebg-preview.png";
+import cart_icon from "../Assest/icon/cart-regular-24.png";
+
+
+function Navbar() {
   return (
-    <div className='navbar'>
-      <div className="nav-logo">
-        <img src="" alt="" />
+    <div className='Navbar'>
+      <div className='header'>
+        <p> Jharna Electronics & Travels</p>
+      </div>
+      <div className='navbar'>
+        <img src={logo} alt="" />
+        <div className="left">
+          <div className="nav-list">
+            <a href="#">Home</a>
+            <a href="#">Services</a>
+            <a href="#">Car Booking</a>
+            <a href="#">Contact</a>
+            <a href="#">About Us</a>
+          </div>
+          <a href="#"><img src={cart_icon} alt="" id="cart"/></a>
+          <div className="login-signup">
+            <a href="#">Login/Signup</a>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
